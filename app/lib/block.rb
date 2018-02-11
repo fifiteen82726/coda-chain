39c036572fe5bdc302a28686177fd42ba345a9c6
fifiteen82726 <fifiteen82726@gmail.com>
2018-02-12 02:25:29 +0800

    Create Block and test.rb

---
+++ app/lib/block.rb
@@ -0,0 +1,18 @@
+class Block
+  def initialize(timestamp, last_hash, hash_value, data)
+    @timestamp = timestamp
+    @last_hash = last_hash
+    @hash_value = hash_value
+    @data = data
+  end
+
+
+  def explain
+    p "==== Block ==== "
+    p "Timestamp: #{@timestamp}"
+    p "last_hash: #{@last_hash}"
+    p "hash_value: #{@hash_value}"
+    p "data: #{@data}"
+    p "==== Block ==== "
+  end
+end
---
+++ app/lib/test.rb
@@ -0,0 +1,8 @@
+require_relative 'block.rb'
+
+class Test < Block
+end
+
+t = Test.new(1,2,3,4)
+t.explain
+
