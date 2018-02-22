62fa18c448af9f953da2ad0662268e24eca1409c
fifiteen82726 <fifiteen82726@gmail.com>
2018-02-22 23:43:48 +0800

    Add: comment

--- app/lib/test.rb
+++ app/lib/test.rb
@@ -3,11 +3,15 @@ require_relative 'block.rb'
 class Test < Block
 end
 
+# Print the block information
 # t = Test.new(1,2,3,4)
 # t.explain
 
+# Create first block
 # t = Test.genesis
 # t.explain
 
-t = Test.mine_block(Test.genesis, '12312')
-t.explain
+
+# Create the next block
+# t = Test.mine_block(Test.genesis, '12312')
+# t.explain
